6d9c610178b70bad6c5f3bf04aa671f29a20cb1a
Lyle Patterson <lyle.patterson2002@gmail.com>
2023-06-18 00:31:25 +0100

    Add project files.

---
+++ Calculator/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        bool endApp = false;
+
+        Console.WriteLine("Console Calculator in C#\r");
+        Console.WriteLine("------------------------\n");
+
+        while (!endApp)
+        {
+            string numInput1 = "";
+            string numInput2 = "";
+            double result = 0;
+
+            Console.Write("Type a number, and then press Enter: ");
+            numInput1 = Console.ReadLine();
+
+            double cleanNum1 = 0;
+            while(!double.TryParse(numInput1, out cleanNum1))
+            {
+
+            }
+        }
+    }
+}
